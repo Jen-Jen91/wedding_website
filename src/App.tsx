6e50365f816472156e0d5ff5001fa15dce3aa476
Jennifer <jen_p_13@hotmail.co.uk>
2021-10-29 16:43:10 +0100

    added basic components and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/nav/NavBar";
+import "./App.css";
+
 const App = () => {
   return (
     <div>
-      <p>Hello world!</p>
+      <NavBar />
+
+      <div className="main">
+        <p>Main</p>
+      </div>
+
+      <Footer />
     </div>
   );
 };
---
+++ src/components/nav/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <p>Jen & Tomek Wedding</p>
+    </nav>
+  );
+};
+
+export default NavBar;
