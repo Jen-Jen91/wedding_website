d08673fe4f8624ecf4857bd3d9dad20fc99e7b3f
Jennifer <jen_p_13@hotmail.co.uk>
2021-12-17 14:44:42 +0000

    updated Footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <p className="footer-copyright-text">&copy; 2021 Jennifer Proctor</p>
+      <p className="footer-name-text">Jen & Tomek Wedding</p>
+      <p className="footer-venue-text">Cambo Estate</p>
     </div>
   );
 };
