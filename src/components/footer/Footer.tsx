4167ba870825c4af4776121ed0e8db7979420ad3
Jennifer <jen_p_13@hotmail.co.uk>
2022-01-04 15:52:28 +0000

    added links to Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,18 @@
-import React from "react";
 import "./Footer.css";
-
-// TODO: add 'Back To Top' button
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer-copyright-text">&copy; 2021 Jennifer Proctor</p>
-      <p className="footer-name-text">Jen & Tomek Wedding</p>
-      <p className="footer-venue-text">Cambo Estate</p>
+      <a
+        className="footer-name-text"
+        href="https://www.jenandtomekwedding.co.uk/"
+      >
+        Jen & Tomek Wedding
+      </a>
+      <a className="footer-venue-text" href="https://www.camboestate.com/">
+        Cambo Estate
+      </a>
     </footer>
   );
 };
