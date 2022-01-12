3473a2eef24ae16b702da52882c3128379899e06
Jennifer <jen_p_13@hotmail.co.uk>
2022-01-12 09:11:55 +0000

    fixed dynamic image sizing for Safari browser

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,18 +4,21 @@ const Hero = () => {
   return (
     <div className="hero-banner">
       <img
+        className="hero-img"
         src="/images/hero-1.jpg"
         alt="jen-and-tomek-1"
         width="33.33%"
         height="auto"
       />
       <img
+        className="hero-img"
         src="/images/hero-2.jpg"
         alt="jen-and-tomek-2"
         width="33.33%"
         height="auto"
       />
       <img
+        className="hero-img"
         src="/images/hero-3.jpg"
         alt="jen-and-tomek-3"
         width="33.33%"
